629f849d754ae6f9251b4350817b114984e1afce
Thaomai Nguyen <thaomaicnguyen@gmail.com>
2022-08-29 13:57:05 -0700

    first commit, created chessboard component

---
+++ src/chessboard/Chessboard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Chessboard() {
+//   const horizontalTiles = ["a", "b", "c", "d", "e", "f", "g", "h"];
+//   const verticalTiles = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  return (
+    <>
+      <div id="chessboard" className="container h-100 d-flex align-items-center justify-content-center" style={ {background: '#7F6239'} }>
+        i am the chessboard! -.-
+      </div>
+    </>
+  )
+}
+
+
+export default Chessboard;
